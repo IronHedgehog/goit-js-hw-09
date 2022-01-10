b99d91f47d071af46ffb0b192f2dabae30c62bba
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2022-01-10 14:23:08 +0200

    color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+refs = {
+  startButton: document.querySelector('[data-start]'),
+  stopButton: document.querySelector('[data-stop]'),
+};
 
+refs.startButton.addEventListener('click', startSwitchColors);
+refs.stopButton.addEventListener('click', stopSwitchColors);
+let switchColorID = null;
+
+function startSwitchColors() {
+  switchColorID = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+    console.log(document.body.style.backgroundColor);
+  }, 1000);
+  refs.startButton.setAttribute('disabled', 'disabled');
+}
+
+function stopSwitchColors() {
+  clearInterval(switchColorID);
+  refs.startButton.removeAttribute('disabled');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
