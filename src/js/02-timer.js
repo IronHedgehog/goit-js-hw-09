122b7d063c5ba5bfc0522426c1713a99a4aa8edd
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2022-01-10 16:53:31 +0200

    timer

---
+++ src/js/02-timer.js
@@ -0,0 +1,70 @@
+import flatpickr from 'flatpickr';
+
+import 'flatpickr/dist/flatpickr.min.css';
+
+import Notiflix from 'notiflix';
+
+const refs = {
+  startButton: document.querySelector('[data-start]'),
+  days: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  second: document.querySelector('[data-seconds]'),
+};
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates[0] >= new Date()) {
+      refs.startButton.removeAttribute('disabled');
+    } else {
+      refs.startButton.setAttribute('disabled', 'disabled');
+      Notiflix.Notify.failure('Please choose a date in the future');
+    }
+
+    refs.startButton.addEventListener('click', e => {
+      let newTime = selectedDates[0] - new Date();
+      setInterval(() => {
+        newTime -= 1000;
+        if (newTime >= 0) {
+          timer(convertMs(newTime));
+        }
+      }, 1000);
+    });
+  },
+};
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = addLeadingZero(Math.floor(ms / day));
+  // Remaining hours
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  // Remaining minutes
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  // Remaining seconds
+  const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
+
+  return { days, hours, minutes, seconds };
+}
+
+function timer({ days, hours, minutes, seconds }) {
+  refs.days.textContent = `${days}`;
+  refs.hours.textContent = `${hours}`;
+  refs.minutes.textContent = `${minutes}`;
+  refs.second.textContent = `${seconds}`;
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+flatpickr('#datetime-picker', options);
